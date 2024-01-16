4c6b904188acfda3b65053fab2ca8537f5c6b6b5
Daniel Echeverria <danijanki122407agmail.com>
2024-01-16 15:10:35 -0600

    hola

--- src/navigation.js
+++ src/navigation.js
@@ -1,6 +1,7 @@
 window.addEventListener('DomContentLoaded',navigator, false);
 window.addEventListener('hashchange',navigator, false);
 
+
 function navigator() {
     console.log({location});
     
@@ -25,9 +26,17 @@ function trendsPage(){
 }
 function searchPage(){
     console.log('Search');
+    main_detail_movies.classList.add('inactive')
+    header_search.classList.remove('inactive')
+    main_section_movies.classList.add('inactive')
+    main_categories.classList.add('inactive')
 }
 function moviePage(){
     console.log('Movie');
+    main_detail_movies.classList.remove('inactive')
+    header_search.classList.add('inactive')
+    main_section_movies.classList.add('inactive')
+    main_categories.classList.add('inactive')
 }
 function CategoriesPage(){
     console.log('Categories');
@@ -36,4 +45,8 @@ function homePage(){
     console.log('Home');
     getTrendingMoviesPreview()
     getCategoriesPreview()
+    main_detail_movies.classList.add('inactive')
+    header_search.classList.remove('inactive')
+    main_section_movies.classList.remove('inactive')
+    main_categories.classList.remove('inactive')
 }
---
+++ src/node.js
@@ -0,0 +1,4 @@
+const header_search = document.querySelector('.header-search')
+const main_detail_movies = document.querySelector('.main-detail-movies')
+const main_section_movies = document.querySelector('.main-section-movies')
+const main_categories = document.querySelector('.main-categories')
